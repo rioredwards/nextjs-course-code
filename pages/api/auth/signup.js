e9e0447dcfd16fbbb59ea08ae331aaf429e649ef
Rio Edwards <rioredwards@gmail.com>
2023-03-06 17:55:15 -0800

    Create users and store in db

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,34 @@
+import { hashPassword } from "../../../lib/auth.js";
+import { connectToDatabase } from "../../../lib/db.js";
+
+async function handler(req, res) {
+  if (req.method !== "POST") return;
+
+  const data = req.body;
+  const { email, password } = data;
+
+  if (
+    !email ||
+    !email.includes("@") ||
+    !password ||
+    password.trim().length < 7
+  ) {
+    res.status(422).json({
+      message: "Invalid input - Password should be at least 7 characters long.",
+    });
+    return;
+  }
+
+  const client = await connectToDatabase();
+  const db = client.db();
+
+  const hashedPassword = await hashPassword(password);
+
+  const result = await db
+    .collection("users")
+    .insertOne({ email: email, password: hashedPassword });
+
+  res.status(201).json({ message: "Created user!" });
+}
+
+export default handler;
