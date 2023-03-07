0456b140c61cfe6dbb7011a084c118abfc859630
Rio Edwards <rioredwards@gmail.com>
2023-03-06 18:51:45 -0800

    Add logout and change navLinks according to signIn status

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,5 +1,6 @@
 import { useState, useRef } from "react";
 import classes from "./auth-form.module.css";
+import { signIn } from "next-auth/client";
 
 async function createUser(email, password) {
   const response = await fetch("/api/auth/signup", {
@@ -38,6 +39,13 @@ function AuthForm() {
 
     if (isLogin) {
       // login
+      const result = await signIn("credentials", {
+        redirect: false,
+        email: enteredEmail,
+        password: enteredPassword,
+      });
+
+      console.log(result);
     } else {
       // signup
       try {
